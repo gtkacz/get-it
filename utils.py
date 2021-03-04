30d6418507459d74edf393884110d1db855077db
Tkacz <gabrielmt2@al.insper.edu.br>
2021-03-04 17:42:18 -0300

    criando o novo repo

---
+++ utils.py
@@ -0,0 +1,53 @@
+from pathlib import Path
+import json
+
+def extract_route(string):
+    return string.split()[1][1:]
+
+def is_path(subject):
+    if not type(subject) is Path:
+        return Path(subject)
+    else:
+        return subject
+        
+def read_file(path):
+    path=is_path(path)
+    extension=path.suffix
+    target=['.txt', '.html', '.css', '.js']
+    if extension in target:
+        with open(path, 'rt', encoding='utf-8') as file:
+            data=file.read()
+        return data
+    else:
+        with open(path, 'rb') as file:
+            data=file.read()
+        return data
+    
+def has_directory(string, directory):
+    c=0
+    for i in string:
+        if i == '/':
+            c+=1
+    if c>1:
+        return string
+    else:
+        return Path(f'./{directory}/{string}')
+
+def load_data(path):
+    path=has_directory(path, 'data')
+    with open(path, encoding='utf-8') as file: 
+        data=json.load(file)
+    return data
+
+def load_template(name):
+    path=has_directory(name, 'templates')
+    return read_file(path)
+
+def build_response(body='', code=200, reason='OK', headers=''):
+    args = [str(code), reason]
+    response = 'HTTP/1.1 ' + (' '.join(args))
+    if headers == '':
+        response += '\n\n' + body
+    else:
+        response += '\n' + headers + '\n\n' + body
+    return response.encode(encoding='UTF-8')
---
+++ views.py
@@ -0,0 +1,40 @@
+from utils import load_data, load_template
+from urllib.parse import unquote_plus
+from utils import has_directory, build_response
+import json
+
+def request_params(request):
+    request = request.replace('\r', '')
+    partes = request.split('\n\n')
+    corpo = partes[1]
+    params = {}
+    for valor in corpo.split('&'):
+        key=unquote_plus(valor.split('=')[0])
+        value=unquote_plus(valor.split('=')[1])
+        params[key]=value
+    return params
+    
+def write_json(data, filename):
+    path=has_directory(filename, 'data')
+    with open(path,'r+', encoding ='utf-8') as file:
+        data=(json.load(file)).append(data)
+        json.dump(data, file, ensure_ascii=False, indent=4) 
+
+def index(request):
+    if request.startswith('GET'):
+        # Cria uma lista de <li>'s para cada anotação
+        # Se tiver curiosidade: https://docs.python.org/3/tutorial/datastructures.html#list-comprehensions
+        note_template = load_template('components/note.html')
+        notes_li = [
+            note_template.format(title=dados['titulo'], details=dados['detalhes'])
+            for dados in load_data('notes.json')
+        ]
+        notes = '\n'.join(notes_li)
+        return build_response() + load_template('index.html').format(notes=notes).encode(encoding='utf-8')
+    
+    elif request.startswith('POST'):
+        params = request_params(request)
+        
+        write_json(params)
+        return build_response(code=303, reason='See Other', headers='Location: /')
+            
