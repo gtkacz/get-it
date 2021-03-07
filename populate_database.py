355532630bd165af6c869e615c67aa590365dadd
Tkacz <gabrielmt2@al.insper.edu.br>
2021-03-07 15:38:37 -0300

    pop function

--- populate_database.py
+++ populate_database.py
@@ -1,12 +1,19 @@
 from database import Database, Note
 from utils import load_data
+import os
 
-db = Database('notes')
+def POPULATE_DB(DB_NAME):
+    DB_PATH=DB_NAME + '.db'
+    if os.path.exists(DB_PATH):
+        os.remove(DB_PATH)
+    
+    db = Database(DB_NAME)
+    json_path = load_data('./data/notes.json')
 
-json_path = load_data('./data/notes.json')
-
-for i in json_path:
-    annotation = Note()
-    annotation.title = list(i.values())[0]
-    annotation.content = list(i.values())[1]
-    db.add(annotation)
+    for i in json_path:
+        annotation = Note()
+        annotation.title = list(i.values())[0]
+        annotation.content = list(i.values())[1]
+        db.add(annotation)
+        
+POPULATE_DB('notes')
