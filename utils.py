9e379bdad6b530983dcd10b415722a2183c79b38
Tkacz <gabrielmt2@al.insper.edu.br>
2021-03-07 01:00:00 -0300

    a mimir

--- utils.py
+++ utils.py
@@ -15,44 +15,44 @@ def is_path(subject):
         return subject
         
 def read_file(path):
-    path=is_path(path)
-    extension=path.suffix
-    target=['.txt', '.html', '.css', '.js']
+    path = is_path(path)
+    extension = path.suffix
+    target = ['.txt', '.html', '.css', '.js']
     if extension in target:
-        with open(path, 'rt', encoding='utf-8') as file:
-            data=file.read()
-        return data.encode(encoding='UTF-8')
+        with open(path, 'rt', encoding = 'utf-8') as file:
+            data = file.read()
+        return data.encode(encoding = 'UTF-8')
     else:
         with open(path, 'rb') as file:
-            data=file.read()
+            data = file.read()
         return data
     
 def has_directory(string, directory):
-    c=0
-    string=str(string)
+    c = 0
+    string = str(string)
     for i in string:
         if i == '/':
-            c+=1
-    if c>1:
+            c += 1
+    if c > 1:
         return str(string)
     else:
         return str(f'./{directory}/{string}')
 
 def load_data(path):
-    path=has_directory(path, 'data')
-    with open(path, encoding='utf-8') as file: 
-        data=json.load(file)
+    path = has_directory(path, 'data')
+    with open(path, encoding = 'utf-8') as file: 
+        data = json.load(file)
     return data
 
 def load_template(name):
-    path=has_directory(name, 'templates')
-    return (read_file(path)).decode(encoding='UTF-8')
+    path = has_directory(name, 'templates')
+    return (read_file(path)).decode(encoding = 'UTF-8')
 
-def build_response(body='', code=200, reason='OK', headers=''):
+def build_response(body = '', code = 200, reason = 'OK', headers = ''):
     args = [str(code), reason]
     response = 'HTTP/1.1 ' + (' '.join(args))
     if headers == '':
         response += '\n\n' + body
     else:
         response += '\n' + headers + '\n\n' + body
-    return response.encode(encoding='UTF-8')
+    return response.encode(encoding = 'UTF-8')
